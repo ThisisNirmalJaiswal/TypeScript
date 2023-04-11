7c4af2ffa9d7d309063601a8379c6004fc019460
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-12 00:40:54 +0530

    classes

--- PureTs/dist/index.js
+++ PureTs/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('typescript is here');
+class User {
+    constructor(name, email) {
+        this.city = 'New Delhi';
+        this.email = email;
+        this.name = name;
+    }
+}
+const nirmal = new User('nirmaL@gmail.com', 'Nirmal');
+nirmal.city = 'morena';
--- PureTs/src/index.ts
+++ PureTs/src/index.ts
@@ -1,1 +1,12 @@
-console.log('typescript is here');
+class User {
+  email: string;
+  name: string;
+  readonly city: string = 'New Delhi';
+  constructor(name: string, email: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const nirmal = new User('nirmaL@gmail.com', 'Nirmal');
+// nirmal.city = 'morena';
