60e0bdb9a2992be536b0c41307aa0bc08bc463bb
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-12 00:54:58 +0530

    Access Modifier

--- PureTs/dist/index.js
+++ PureTs/dist/index.js
@@ -14,7 +14,23 @@ class User {
     ) {
         this.name = name;
         this.email = email;
+        this._courseCount = 1;
         this.city = 'New Delhi';
+    }
+    deleteToken() {
+        console.log('Token Deleted');
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNum;
     }
 }
 const nirmal = new User('nirmaL@gmail.com', 'Nirmal');
--- PureTs/src/index.ts
+++ PureTs/src/index.ts
@@ -10,11 +10,31 @@
 
 // another way to define a class
 class User {
+  private _courseCount = 1;
+
   readonly city: string = 'New Delhi';
   constructor(
     public name: string,
     public email: string // private userId: string
   ) {}
+
+  private deleteToken() {
+    console.log('Token Deleted');
+  }
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error('Course count should be more than 1');
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const nirmal = new User('nirmaL@gmail.com', 'Nirmal');
