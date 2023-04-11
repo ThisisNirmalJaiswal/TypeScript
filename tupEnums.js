2be90665f569643c9d4d2ee36d0e522785653109
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-11 17:46:26 +0530

    tupls

---
+++ tupEnums.js
@@ -0,0 +1,7 @@
+// const user: (string | number)[] = [1, 'nirmal'];
+var user;
+user = ['nirmal', 12, true];
+var rgb = [125, 255, 0.3];
+var newUser = [112, 'nirmal@gmail.com'];
+newUser[1] = 'hc.com';
+// newUser.push(true);
