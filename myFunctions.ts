56d43ac32ce28a4ecf22602829db2e47e734103e
Nirmal Jasval <nirmaljaiswal7746@gmail.com>
2023-04-11 15:15:43 +0530

    functions2

--- myFunctions.ts
+++ myFunctions.ts
@@ -18,4 +18,35 @@ function loginUser(name: String, email: string, isPaid: boolean = false) {
 loginUser('nirmal', 'nirm@example.com');
 signUpUser('nirmal', 'nirm@example.com', true);
 
+// function getValue(myVal: number): boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return '200 OK';
+// }
+
+// getValue(20);
+
+// const getHello = (s: String): string => {
+//   return '';
+// };
+
+const heroes = ['Thor', 'Iron Man', 'Spiderman'];
+
+// const heroes = [1, 2, 3];
+
+heroes.map((hero) => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errMsg: string): void {
+  console.log(errMsg);
+}
+
+function handleError(errMsg: string): never {
+  throw new Error(errMsg);
+}
+
 export {};
+
+8750031891;
